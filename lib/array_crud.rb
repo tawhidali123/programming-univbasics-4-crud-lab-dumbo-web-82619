cd1a7347bc66d18510e5e6e48d92a1ce932f26b6
Tawhid Ali <tawhidali126@gmail.com>
2019-07-31 01:55:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = ["this", "is", "my", "array"]
+  things = ["this", "is", "my", "array"]
 end
 
 def add_element_to_end_of_array(array, element)
