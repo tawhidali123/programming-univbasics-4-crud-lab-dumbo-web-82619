a2da9f171f35a2b82ce28c8deaa377ba7111d062
Tawhid Ali <tawhidali126@gmail.com>
2019-07-31 01:56:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,13 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = ["this", "is", "my", "array"]
-  array.unshift("beginning")
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  array = ["this", "is", "my", "array"]
-  array.pop
+  array.pop(element)
 end
 
 def remove_element_from_start_of_array(array)
